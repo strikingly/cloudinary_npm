a5365a3b4c73b6f279494d1fb3f36ce326e22d14
Dafeng Guo <dfguo.joe@gmail.com>
2016-01-10 14:02:30 +0800

    add utils

--- cloudinary.js
+++ cloudinary.js
@@ -1,5 +1,6 @@
 var _ = require('lodash'),  cloudinary = module.exports;
 exports.config = require("./lib/config");
+exports.utils = require("./lib/utils");
 
 exports.url = function(public_id, options) {
   options = _.extend({}, options);
